8fc6a770da60c388f2fe76bde80f3158cf2eec3b
mwarnck <mirco.warnck@googlemail.com>
2022-06-12 15:28:04 +0200

    All in one commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">Unsere ToDos</h1>
+      <p class="text-xl" v-if="openTodos.length > 0">
+        Offene ToDos: {{ openTodos.length }}
+      </p>
+      <p class="text-xl" v-else>Keine ToDos, gratuliere!</p>
+      <div class="mt-4">
+        <input type="text" class="py-2 w-2/3 text-black" v-model="newTodo" />
+        <button class="bg-red-400 py-2 w-1/3" @click="addTodo">Add ToDo</button>
+      </div>
+    </div>
+    <div v-for="(todo, index) in todos" :key="todo.todo">
+      <SingleTodo
+        :todoprop="todo"
+        :todoindex="index"
+        @toggledone-index="toggleDone"
+        @removetodo-index="deleteTodo"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SingleTodo from "./components/SingleTodo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      newTodo: "",
+      todos: [{ todo: "Sport", done: false }],
+    };
+  },
+  methods: {
+    toggleDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+      this.storeTodos();
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+      this.storeTodos();
+    },
+    addTodo() {
+      if (this.newTodo.trim() === "") {
+        return;
+      }
+      this.todos.push({ todo: this.newTodo, done: false });
+      this.storeTodos();
+    },
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+  mounted() {
+    let data = localStorage.getItem("todos");
+    if (data !== "" && data !== null) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  },
+  computed: {
+    openTodos() {
+      const openTodos = this.todos.filter((todo) => {
+        return !todo.done;
+      });
+      return openTodos;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    SingleTodo,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SingleTodo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="flex justify-between items-center py-2 px-2"
+    :class="{ 'bg-green-600': this.todo.done, 'bg-red-600': !this.todo.done }"
+  >
+    <h1 class="cursor-pointer" @click="toggleTodo">{{ todo.todo }}</h1>
+    <button class="bg-black py-1 px-1 rounded" @click="removeTodo">
+      Delete ToDo
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleTodo",
+  props: ["todoprop", "todoindex"],
+  data() {
+    return {
+      todo: this.todoprop,
+    };
+  },
+  methods: {
+    toggleTodo() {
+      this.$emit("toggledone-index", this.todoindex);
+    },
+    removeTodo() {
+      this.$emit("removetodo-index", this.todoindex);
+    },
+  },
+};
+</script>
